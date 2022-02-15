edbf3783efec5f6fc7f89e0f9763861e73abcec7
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-02-16 02:26:52 +0900

    HTML書き出し

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ const { task, series } = require('gulp');
 /**
  * タスクモジュール読み込み
  */
+const { NUNJUCKS_BUILD } = require('./tasks/nunjucks-build');
 const { SASS_BUILD } = require('./tasks/sass-build');
 const { IMAGE_MIN } = require('./tasks/image-min');
 
@@ -14,5 +15,6 @@ const { IMAGE_MIN } = require('./tasks/image-min');
 /**
  * タスク定義
  */
+task('nunjucksBuild', series(NUNJUCKS_BUILD));
 task('sassBuild', series(SASS_BUILD));
 task('imageMin', series(IMAGE_MIN));
