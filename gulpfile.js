02ae273446874de515621d02a9d58ae04aed52da
zakzakst <59270506+zakzakst@users.noreply.github.com>
2022-02-15 22:24:49 +0900

    画像圧縮

--- gulpfile.js
+++ gulpfile.js
@@ -8,9 +8,11 @@ const { task, series } = require('gulp');
  * タスクモジュール読み込み
  */
 const { SASS_BUILD } = require('./tasks/sass-build');
+const { IMAGE_MIN } = require('./tasks/image-min');
 
 
 /**
  * タスク定義
  */
 task('sassBuild', series(SASS_BUILD));
+task('imageMin', series(IMAGE_MIN));
---
+++ tasks/image-min.js
@@ -0,0 +1,39 @@
+/**
+ * モジュール読み込み
+ */
+const { src, dest } = require('gulp');
+const imagemin = require('gulp-imagemin');
+const mozjpeg = require('imagemin-mozjpeg');
+const pngquant = require('imagemin-pngquant');
+const changed = require('gulp-changed');
+
+
+/**
+* 変数設定
+*/
+const files = './src/img/**/*.{jpg,jpeg,png,gif,svg}';
+const dist = './dist/img';
+const imageminPlugin = [
+  mozjpeg({
+    quality: 80,
+  }),
+  pngquant({
+    quality: [0.65, 0.8],
+    speed: 1,
+  }),
+  imagemin.svgo(),
+  imagemin.gifsicle(),
+];
+
+
+/**
+* 関数
+*/
+const IMAGE_MIN = () => {
+  return src(files)
+    .pipe(changed(dist))
+    .pipe(imagemin(imageminPlugin))
+    .pipe(dest(dist));
+};
+
+module.exports = { IMAGE_MIN };
